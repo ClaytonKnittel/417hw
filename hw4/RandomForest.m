e99df901951f9d6dbb94ccda2645e08c0ccd61f1
Isabelle <isabelle.xu@wustl.edu>
2019-11-05 01:39:43 -0600

    random

--- hw4/RandomForest.m
+++ hw4/RandomForest.m
@@ -16,4 +16,55 @@ function [oob_err, test_err] = RandomForest(X_tr, y_tr, X_te, y_te, numBags, m)
 %
 % You may use "fitctree" but not "TreeBagger" or any other inbuilt bagging function
 
+n = size(X_tr,1);
+
+oob_err_pts = zeros(n,1); % Holds aggregated predictions
+oob_err_bags = zeros(numBags,1); % Holds OOB in terms of number of bags
+test_err = 0;
+
+for b=1:numBags
+    % Create dataset by sampling n points from D with replacement
+    samples = floor(n * rand(n, 1)) + 1;
+    Db = X_tr(samples,:);
+    yb = y_tr(samples,:);
+    
+    % Learn decision tree t_b
+    t_b = fitctree(Db,yb,'CrossVal','off'); % Holdout??? with cvpartition???
+    
+    % Validation -> Retrieve all training pnts x_i not used to train
+    unused = setdiff(1:n, samples);
+    curr_pred = t_b.predict(X_tr(unused,:));
+    
+    % Save and aggregate prediction results 
+    oob_err_pts(unused) = oob_err_pts(unused) + curr_pred;
+    
+    % Compute plurality vote of the trees that do not train on that point 
+    oob_err_vote = sign(oob_err_pts);
+    
+    % Find and deal with ties
+    tie = ~oob_err_vote;
+    tie_dimen = size(tie,2);
+    % Breaking tie vote arbitrarily
+    % oob_err_vote(tie) = -1; % 
+    oob_err_vote(tie) = datasample([-1,1],tie_dimen);
+    
+    % save each tree's OOB error, which accumulates as we move on and 
+    % include the next tree in voting etc...
+    oob_err_bags(b) = sum(oob_err_vote ~= y_tr)/n;
+    
+    % Plurality vote vs actual labels aggre for test error calc
+    test_err = test_err + (t_b.predict(X_te) ~= y_te);
 end
+
+% Calculate final out of bag error
+oob_err = oob_err_bags(numBags);
+
+test_err = test_err / n;
+
+% Plot out-of-bag-error 
+plot(1:numBags, oob_err_bags);
+ylabel('OOB Error');
+xlabel('Num Bags');
+
+end
+
